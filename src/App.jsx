8bbb500cc005d9979bccc8a53d147ab8cc2d533d
vladislavzmaga <100230151+vladislavzmaga@users.noreply.github.com>
2023-07-26 18:02:20 +0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,90 @@
-import detectEthereumProvider from '@metamask/detect-provider';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import detectEthereumProvider from '@metamask/detect-provider'
 
 import "./App.css";
 import { RepoLink } from "./App.styled";
 import { Form } from "./components/Form/Form";
 import { Header } from "./components/Header/Header";
 
-
-const provider = await detectEthereumProvider();
-
-
 function App() {
+  const [hasProvider, setHasProvider] = useState(null)
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
 
-  
+  useEffect(() => {
+    const getProvider = async () => {
+      const provider = await detectEthereumProvider({ silent: true })
+      setHasProvider(Boolean(provider))
+    }
 
-if (provider) {
-  // From now on, this should always be true:
-  // provider === window.ethereum
-  startApp(provider); // initialize your app
-} else {
-  console.log('Please install MetaMask!');
-}
+    getProvider()
+  }, [])
 
-function startApp(provider) {
-  // If the provider returned by detectEthereumProvider isn't the same as
-  // window.ethereum, something is overwriting it – perhaps another wallet.
-  if (provider !== window.ethereum) {
-    console.error('Do you have multiple wallets installed?');
-  }
-  // Access the decentralized web!
-}
+  const conectionWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const balance = await window.ethereum.request({
+          method: "eth_getBalance",
+          params: [accounts[0], "latest"],
+        });
+        setWalletAddress(accounts[0]);
+        setUserBalance(ethers.formatEther(balance));
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert("Please install MetaMask");
+    }
+  };
+
+  const getCurrentWalletConnected = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (accounts.length > 0) {
+          const balance = await window.ethereum.request({
+            method: "eth_getBalance",
+            params: [accounts[0], "latest"],
+          });
+          setWalletAddress(accounts[0]);
+          setUserBalance(ethers.formatEther(balance));
+        } else {
+          console.log("Connect to MetaMask using the Connect button");
+        }
+      } catch (err) {
+        console.error(err.message);
+      }
+    } else {
+      alert("Please install MetaMask");
+    }
+  };
+
+  const addWalletListener = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        setWalletAddress(accounts[0]);
+      });
+    } else {
+      setWalletAddress("");
+      alert("Please install MetaMask");
+    }
+  };
 
   return (
     <>
-      <Header />
+      <Header
+        walletAddress={walletAddress}
+        userBalance={userBalance}
+        conectionWallet={conectionWallet}
+        addWalletListener={addWalletListener}
+        getCurrentWalletConnected={getCurrentWalletConnected}
+      />
       <Form />
       <RepoLink
         href="https://github.com/vladislavzmaga/test-task-dexola"
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,86 +1,43 @@
-import { ethers } from "ethers";
+import PropTypes from 'prop-types';
 
-
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { ConectedWalletBtn, Logo, StyledHeader } from "./HeaderStyled";
 
-
-export const Header = () => {
-  const [walletAddress, setWalletAddress] = useState(null);
-  const [userBalance, setUserBalance ] = useState(null)
+export const Header = ({
+  walletAddress,
+  userBalance,
+  conectionWallet,
+  addWalletListener,
+  getCurrentWalletConnected,
+}) => {
   useEffect(() => {
     getCurrentWalletConnected();
     addWalletListener();
   }, [walletAddress]);
 
-  const ConectionWallet = async () => {
-    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
-      try {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        const balance = await window.ethereum.request({
-          method: "eth_getBalance", params: [accounts[0], "latest"]
-        })
-        setWalletAddress(accounts[0]);
-        const fixedBalance = parseInt(ethers.formatEther(balance))
-        setUserBalance(fixedBalance.toFixed(3))
-        console.log(fixedBalance.toFixed(3));
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      alert("Please install MetaMask");
-    }
-  };
-
-  const getCurrentWalletConnected = async () => {
-    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
-      try {
-        const accounts = await window.ethereum.request({
-          method: "eth_accounts",
-        });
-        if (accounts.length > 0) {
-          const balance = await window.ethereum.request({
-            method: "eth_getBalance", params: [accounts[0], "latest"]
-          })
-          setWalletAddress(accounts[0]);
-          setUserBalance(ethers.formatEther(balance))
-        } else {
-          console.log("Connect to MetaMask using the Connect button");
-        }
-      } catch (err) {
-        console.error(err.message);
-      }
-    } else {
-      alert("Please install MetaMask");
-    }
-  };
-
-  const addWalletListener = async () => {
-    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
-      window.ethereum.on("accountsChanged", (accounts) => {
-        setWalletAddress(accounts[0]);
-      });
-    } else {
-      setWalletAddress("");
-      alert("Please install MetaMask");
-    }
-  };
 
   return (
     <>
       <StyledHeader>
         <Logo>YOUR WALLET</Logo>
-        <ConectedWalletBtn onClick={ConectionWallet}>
-        {walletAddress && walletAddress.length > 0
-                    ? `${userBalance ? userBalance: 0.0}: ${walletAddress.substring(
-                        0,
-                        5
-                      )}...${walletAddress.substring(38)}`
-                    : "Connect Wallet"}
+        <ConectedWalletBtn onClick={conectionWallet}>
+          {walletAddress && walletAddress.length > 0
+            ? `${userBalance ? userBalance.substring(0, 5) : 0.0}: ${walletAddress.substring(
+                0,
+                5
+              )}...${walletAddress.substring(38)}`
+            : "Connect Wallet"}
         </ConectedWalletBtn>
       </StyledHeader>
     </>
   );
+};
+
+
+Header.propTypes = {
+  conectionWallet: PropTypes.func.isRequired,
+  addWalletListener: PropTypes.func.isRequired,
+  getCurrentWalletConnected: PropTypes.func.isRequired,
+  walletAddress: PropTypes.string,
+  userBalance: PropTypes.string,
 };
